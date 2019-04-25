674c808fc32e29a271e8c730c0c8dbbab2168cb4
carloseduardov8 <carloseduardov8@hotmail.com>
2019-04-25 16:13:45 -0300

    Update question 6

--- question6.py
+++ question6.py
@@ -2,9 +2,9 @@ import numpy as np
 import math
 
 # Parameters:
-alpha = 2
+alpha = 0.5
 a = 1
-b = 2
+b = 8
 n = 10**5
 
 def f(x):
@@ -12,8 +12,8 @@ def f(x):
 
 def sample_h():
 	x = np.random.uniform(a,b)
-	y = np.random.uniform(f(a),f(b))
-	if (f(x) <= y):
+	y = np.random.uniform(0,f(b))
+	if (y <= f(x)):
 		return 1
 	else:
 		return 0
@@ -23,11 +23,11 @@ accumulator = 0
 for i in range(n):
 	accumulator += sample_h()
 
-result = accumulator / n
+result = accumulator*1.0 / n
 print("Result so far: " + str(result))
 
 # Multiplies by area of rectangle [a,b]x[f(a),f(b)]:
-rect_area = (b-a)*(f(b)-f(a))
+rect_area = (b-a)*f(b)
 print("Area of rect: " + str(rect_area))
 result = result * rect_area
 
